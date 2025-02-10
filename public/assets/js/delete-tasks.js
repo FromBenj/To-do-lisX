fc29d78aae5cd1e0c92b767cf4eae86271021e74
Benjamin TOUBOUL <touboul.benjamin@gmail.com>
2025-02-10 18:11:22 +0100

    Opacity for each situation resolved

--- public/assets/js/delete-tasks.js
+++ public/assets/js/delete-tasks.js
@@ -4,6 +4,9 @@ const deleteAllTasks = document.getElementById("delete-all");
 const taskList = document.getElementById("tasks-list");
 const addButton = document.getElementById("add-button");
 const deleteButton = document.getElementsByClassName("delete-button");
+function manageListOpacity() {
+    (taskList.children.length === 0) ? taskList.style.opacity = "0.5" : taskList.style.opacity = "1";
+}
 
 // Delete all tasks
 manageListOpacity();
@@ -18,31 +21,32 @@ addButton.addEventListener("click", () => {
 })
 
 //Delete a task
-function manageListOpacity() {
-    (taskList.children.length === 0) ? taskList.style.opacity = "0.5" : taskList.style.opacity = "1";
-    for (let i = 0; i < deleteButton.length; i++) {
-        deleteButton[i].addEventListener("click", () => {
-            taskList.children.length >= 2 ? taskList.style.opacity = "1" : taskList.style.opacity = "0.5";
-        })
-    }
-}
 
-//delete a new task
-function manageUpdatedListOpacity() {
-    addButton.addEventListener("click", () => {
-        let tasksCount = taskList.children.length + 1;
-        let deleteButtons = document.querySelectorAll(".delete-button");
-        deleteButtons.forEach((button) => {
-            button.addEventListener("click", () => {
-                tasksCount--;
-                tasksCount === 0 ? taskList.style.opacity = "0.5" : null;
-            })
-        })
+for (let i = 0; i <  taskList.children.length; i++) {
+    deleteButton[i].addEventListener("click", () => {
+        taskList.children.length > 1 ? taskList.style.opacity = "1" : taskList.style.opacity = "0.5";
     })
 }
 
-manageUpdatedListOpacity();
+//delete a new task
 
+let taskListLength = taskList.children.length;
+addButton.addEventListener("click", () => {
+    setTimeout(() => {
+        let firstTaskDeleteButton = taskList.querySelector("div .delete-button" );
+        console.log(firstTaskDeleteButton)
+        firstTaskDeleteButton.addEventListener("click", () => {
+            taskList.style.opacity = "0.5";
+        })
+    }, 500);
+
+    taskListLength ++;
+    for (let i = 0; i < taskList.children.length; i++) {
+        deleteButton[i].addEventListener("click", () => {
+            taskList.children.length > 1 ? taskList.style.opacity = "1" : taskList.style.opacity = "0.5";
+        })
+    }
+})
 
 
 
